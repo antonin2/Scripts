d28ee9237b791842cb48998c6a5716cb5744c930
Antonin <78807761+antonin2@users.noreply.github.com>
2021-12-01 18:22:49 +0100

    Create calcul_age_en_secondes.py

---
+++ NSI/calcul_age_en_secondes.py
@@ -0,0 +1,6 @@
+#Script permettant de calculer le nombre de secondes écoulées depuis sa naissance.
+
+age = int(input("Votre âge :"))
+secondes = age*365*24*60*60
+print("Le nombre de secondes écoulées depuis votre naissance est de : ")
+print(secondes)
